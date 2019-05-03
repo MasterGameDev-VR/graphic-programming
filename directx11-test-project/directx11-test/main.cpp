1cb81ef589af23077c2be15ae7e3e4d3a4c470b3
Andrea-Cancellieri <andrea.cancellieri95@libero.it>
2019-05-03 19:39:29 +0200

    Revert "Graphics Programming Homework"
    This reverts commit d81aa6b30ddbe7f29e6bad0e1cb40763f1e77c77.

--- directx11-test-project/directx11-test/main.cpp
+++ directx11-test-project/directx11-test/main.cpp
@@ -1,7 +1,7 @@
 #include "stdafx.h"
 #include <demo/box_demo/box_demo_app.h>
 #include <demo/lights_demo/lights_demo_app.h>
-#include <demo/textures_demo/textures_demo_app.h>
+
 
 using namespace xtest::application;
 
@@ -24,7 +24,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	directxSettings.showFrameStats = true;
 	
 
-	typedef xtest::demo::TexturesDemoApp TestApp;
+	typedef xtest::demo::LightsDemoApp TestApp;
 
 	TestApp app(hInstance, windowSettings, directxSettings);
 	app.Init();
