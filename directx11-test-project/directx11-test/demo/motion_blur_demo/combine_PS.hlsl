b6477bae9245bf26843891db643c803d263f003c
Federico Ciardi <fedeilre@yahoo.it>
2019-06-17 17:55:28 +0200

    WORKING!!!!!

--- directx11-test-project/directx11-test/demo/motion_blur_demo/combine_PS.hlsl
+++ directx11-test-project/directx11-test/demo/motion_blur_demo/combine_PS.hlsl
@@ -10,8 +10,25 @@ struct VertexOut
 };
 
 float4 main(VertexOut pin) : SV_TARGET {
-	float3 col = colorTexture.Sample(textureSampler, pin.uv.xy).rgb;
-	float2 vel = motionBlurTexture.Sample(textureSampler, pin.uv.xy).rg;
+	float w;
+	float h;
+	colorTexture.GetDimensions(w, h);
+	float2 texelSize = float2(1.0f, 1.0f) / float2(w, h);
+	float2 screenTexCoords = pin.pos.xy * texelSize;
 
-	return float4(col.x, col.y, col.z, 1.0f);
+	float2 vel = motionBlurTexture.Sample(textureSampler, pin.uv.xy).rg;
+	vel *= 1.2f;
+
+	float speed = length(vel / texelSize);
+	int nSamples = clamp(int(speed), 1, 5);
+
+	float3 finalColor = colorTexture.Sample(textureSampler, pin.uv.xy).rgb;
+	[unroll]
+	for (int i = 1; i < nSamples; ++i) {
+		float2 offset = vel * (float(i) / float(nSamples - 1) - 0.5);
+		finalColor += colorTexture.Sample(textureSampler, pin.uv.xy + offset).rgb;
+	}
+	finalColor /= float(nSamples);
+
+	return float4(finalColor, 1.0f);
 }
